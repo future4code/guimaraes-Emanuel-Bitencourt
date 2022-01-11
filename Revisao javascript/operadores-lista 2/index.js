67ef7643ace1420c58d3d631ecb7c8dce3dbd509
emanuel433 <emanuelaraujo30@yahoo.com.br>
2022-01-11 20:21:30 -0300

    variaveis e operadores

---
+++ Revisao javascript/operadores-lista 2/index.js
@@ -0,0 +1,19 @@
+// *************************** OPERADORES E LÓGICA DE PROGRAMAÇÃO ****************\\
+
+// Exercicio de interpretação
+
+/*
+1.const bool1 = true                Está sendo declarada uma constante boleana nomeada  de bool1 e seu valor atribuido é true
+const bool2 = false                 Está sendo declarada uma constante boleana nomedada de bool2 e seu valor atribuido é false
+const bool3 = !bool2                Está sendo declarada uma constante boleana nomedada de bool3 e seu valor atribuido é diferente de bool2 
+
+let resultado = bool1 && bool2      
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
