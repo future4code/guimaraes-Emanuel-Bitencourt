1891b2e0ed60cf2684c2024a84dc6d9c1f04d118
emanuel433 <emanuelaraujo30@yahoo.com.br>
2022-03-01 20:59:11 -0300

    por aqui eu paro

---
+++ modulo3/rend-condicional-form/labenuforms/src/components/Final.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+ class App extends React.Component{
+  return (
+    <div>
+      <h3>O FORMULÁRIO ACABOU</h3>
+      <p>Muito obrigado por participar! Entraremos em contato!</p>
+    </div>
+  );
+}
